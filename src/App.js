4e244f7336164fff06e7ec2ed64ec5d98790ebc1
Amber Phair <amphair@outlook.com>
2020-12-22 11:14:04 -0500

    Adding save ability
    Added ability to save generated image to your computer.
    Added functionality to keep fetching more cats from Pexels api. Will fetch 50 at a time and only re-call api when empty.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import React, { useState } from 'react';
 import './App.css';
 import RandomCatService from './RandomCatService';
 import Cat from './Cat';
+import * as htmlToImage from 'html-to-image';
+import { saveAs } from 'file-saver';
 
 const App = ({catName}) => {
 
   const meows = ['meow.wav', 'meow2.wav', 'meow3.mp3', 'meow4.wav', 'meow5.wav'];
   const [addedCats, setAddedCats] = useState([]);
 
-  const sayHello = () => {
+  const addMoreCats = async () => {
     const rand = Math.floor((Math.random() * 5));
     const rando = meows[rand];
     const audio = new Audio(rando);
     audio.play();
-    // api key 563492ad6f917000010000010fbfa80c6d554b588eae0d5aee13f840 
-    setAddedCats([...addedCats, RandomCatService.addCat()]);
+    try {
+      let cat = await RandomCatService.addCat();
+      setAddedCats([...addedCats, cat]);
+    }
+    catch(error){
+        //something wrong http
+    }
   }
 
-  console.log(addedCats);
+  const save = () => {
+    htmlToImage.toBlob(document.getElementById('cat-background'), {width: window.innerWidth, height: window.innerHeight})
+    .then(function (blob) {
+      saveAs(blob, "cat-background.png");
+    });
+  }
+
   return (
     <div className="App">
-      <button type="button" className="meow-btn" onClick={sayHello}>Meow</button>
-      <div>
+      <button type="button" className="meow-btn" onClick={addMoreCats}>Meow</button>
+      <button type="button" className="save-btn" onClick={save}>Save</button>
+      <div id="cat-background">
         {addedCats.map((cat, index) => <div key={index}><Cat cat={cat}/></div> )}
       </div>
     </div>
--- src/RandomCatService.js
+++ src/RandomCatService.js
@@ -1,20 +1,50 @@
 
-const availableCats = ["./cat1.jpg", "./cat2.jpg", "./cat3.jpg"];
+import axios from 'axios';
+
+let newAvailableCats = [];
+let lastPage = 1;
+const API_KEY = process.env.REACT_APP_PEXELS_API_KEY;
 
 const RandomCatService = {
     
-    sayHello: function() {
-        return 'Hello ffff';
+    addCat: async function() {        
+        if(newAvailableCats.length === 0){
+            try{
+                const response = await this.getMoreCats();
+                lastPage++;
+                response.data.photos.forEach((data) => {
+                    newAvailableCats.push(data.src.medium);
+                });
+            }
+            catch (error) {
+                //any http error
+                // throw something ?
+            }
+            return this.getRandoCat();
+        }
+        else{
+            return this.getRandoCat();
+        }        
     },
 
-    addCat: function() {
-        const rand = Math.floor((Math.random() * 3));
+    getRandoCat: function() {
+        const rand = Math.floor((Math.random() * newAvailableCats.length));
         const randomAngle = Math.floor((Math.random() * 360));
-        console.log(window.innerWidth, window.innerHeight);
         let randomX = Math.floor((Math.random() * window.innerWidth));
         let randomY = Math.floor((Math.random() * window.innerHeight));
 
-        return {url: availableCats[rand], angle: randomAngle, x: randomX, y: randomY};
+        let cat = newAvailableCats[rand];
+
+        newAvailableCats.splice(rand, 1);
+
+        return {url: cat, angle: randomAngle, x: randomX, y: randomY};
+    },
+
+    getMoreCats: async function() {
+        return axios.get("https://api.pexels.com/v1/search", {
+            params: {query: "Cat", per_page: 50, page: lastPage},
+            headers: {"Authorization": API_KEY}
+        });
     }
 }
 
